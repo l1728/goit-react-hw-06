21e1f429ccf125fb8904da8af5a8c5db3a01c12f
Tetyana Gogulya <cheburashka077@ukr.net>
2024-06-16 02:02:13 +0300

    init

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,34 +1,45 @@
 import styles from './App.module.css';
-import { useState, useEffect } from 'react';
+// import { useState, useEffect } from 'react';
 import ContactForm from '../ContactForm/ContactForm';
 import SearchBox from '../SearchBox/SearchBox';
 import ContactList from '../ContactList/ContactList';
+import { useSelector, useDispatch } from 'react-redux';
+import {
+  addContact,
+  deleteContact,
+  selectContacts,
+} from '../../redux/contactsSlice';
+import { changeFilter, selectNameFilter } from '../../redux/filtersSlice';
+
 import 'izitoast/dist/css/iziToast.min.css';
 import iziToast from 'izitoast';
 
-const initialContacts = [
-  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-];
+// const initialContacts = [
+//   { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+//   { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+//   { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+//   { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+// ];
 
 const App = () => {
   // Ініціалізація стану з локального сховища або початкових контактів
-  const [contacts, setContacts] = useState(() => {
-    const savedContacts = localStorage.getItem('contacts');
-    return savedContacts ? JSON.parse(savedContacts) : initialContacts;
-  });
+  // const [contacts, setContacts] = useState(() => {
+  //   const savedContacts = localStorage.getItem('contacts');
+  //   return savedContacts ? JSON.parse(savedContacts) : initialContacts;
+  // });
 
-  const [searchQuery, setSearchQuery] = useState('');
+  // const [searchQuery, setSearchQuery] = useState('');
 
   // Збереження контактів у localStorage при зміні списку контактів
-  useEffect(() => {
-    localStorage.setItem('contacts', JSON.stringify(contacts));
-  }, [contacts]);
+  // useEffect(() => {
+  //   localStorage.setItem('contacts', JSON.stringify(contacts));
+  // }, [contacts]);
 
-  // Функція для додавання нового контакту
-  const addContact = newContact => {
+  const dispatch = useDispatch();
+  const contacts = useSelector(selectContacts);
+  const searchQuery = useSelector(selectNameFilter);
+
+  const handleAddContact = newContact => {
     if (
       contacts.some(
         contact => contact.name.toLowerCase() === newContact.name.toLowerCase()
@@ -48,21 +59,17 @@ const App = () => {
       });
       return;
     }
-    setContacts(prevContacts => [...prevContacts, newContact]);
+    dispatch(addContact(newContact));
   };
 
-  // Функція для оновлення пошукового запиту
   const handleSearch = query => {
-    setSearchQuery(query);
-  };
-  // Функція для видалення контакта з певним id
-  const deleteContact = contactId => {
-    setContacts(prevContacts =>
-      prevContacts.filter(contact => contact.id !== contactId)
-    );
+    dispatch(changeFilter(query));
   };
 
-  // Фільтрація контактів на основі пошукового запиту
+  const handleDeleteContact = contactId => {
+    dispatch(deleteContact(contactId));
+  };
+
   const filteredContacts = contacts.filter(contact =>
     contact.name.toLowerCase().includes(searchQuery.toLowerCase())
   );
@@ -70,11 +77,11 @@ const App = () => {
   return (
     <div>
       <h1 className={styles.h1}>Phonebook</h1>
-      <ContactForm addContact={addContact} />
+      <ContactForm addContact={handleAddContact} />
       <SearchBox searchQuery={searchQuery} handleSearch={handleSearch} />
       <ContactList
         contacts={filteredContacts}
-        onDeleteContact={deleteContact}
+        onDeleteContact={handleDeleteContact}
       />
     </div>
   );
